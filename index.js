5f5ee58b21b12a882ec56957dcd026113f8ec43a
Aaron Dov Turkel <AaronDovTurkel@gmail.com>
2020-06-04 16:40:22 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+module.exports = plugin;
+
+function plugin(config){
+    return function(files, metalsmith, done) {
+        setImmediate(done);
+        Object.keys(files).forEach(function(file) {
+            const data = files[file];
+            const path = data.path;
+            console.log(path);
+           if (path in config) {
+               Object.keys(config[path]).forEach(configKey => {
+                   if (configKey in data) {
+                       data[configKey] = config[path][configKey] || data[configKey];
+                   }
+               });
+           }
+        });
+    };
+};
+
